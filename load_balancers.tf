fafa00116f2828942a222878652aeec5f8d7d24d
obingo77 <56453225+obingo77@users.noreply.github.com>
2022-01-14 20:36:01 +0000

    Update load_balancers.tf

---
+++ load_balancers.tf
@@ -0,0 +1,31 @@
+resource "aws_lb" "test" {
+  name               = "test-lb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.alb.id]
+  subnets            = data.aws_subnet_ids.default.ids
+
+  // enable_deletion_protection = true
+
+
+  tags = {
+    Environment = "test"
+  }
+}
+
+resource "aws_lb_listener" "front_end" {
+  load_balancer_arn = aws_lb.test.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type = "fixed-response"
+
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "Fixed response content"
+      status_code  = "200"
+    }
+  }
+}
+
